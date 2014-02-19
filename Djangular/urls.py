a762218cddba2745aa12a54692c910e8c413e6db
Kevin McBrayer <kmcbrayer77@gmail.com>
2014-02-18 21:40:02 -0500

    urls change, kinda working??

--- Djangular/urls.py
+++ Djangular/urls.py
@@ -3,6 +3,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 from django.views.generic import TemplateView
+
+import api
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -10,5 +13,5 @@ urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name="index.html")),
     
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api/', include(api.urls)),
+    url(r'^', include(api)),
 )
